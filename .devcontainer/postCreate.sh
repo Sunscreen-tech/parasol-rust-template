373a2be621b4d9bc2bf255f6495dfc7a14754630
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2023-09-14 11:13:42 -0600

    Make template cargo devcontainer useable

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
-sudo chown vscode:vscode /usr/local/cargo/registry
 cargo install cargo-watch 
+sudo chown -R vscode:vscode /usr/local/cargo/registry
+sudo chown -R vscode:vscode /usr/local/cargo/git
 
 # Force the installation of the git submodules
 git remote add origin https://github.com/Sunscreen-tech/parasol-rust-template.git

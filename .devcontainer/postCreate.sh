de650e75ef85c55caae78477e1169d05ae50a0ad
Ryan Orendorff <12442942+ryanorendorff@users.noreply.github.com>
2023-09-18 15:19:18 -0600

    User based cargo for immediate use

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/env bash
 
-cargo install cargo-watch 
-sudo chown -R vscode:vscode /usr/local/cargo/registry
-sudo chown -R vscode:vscode /usr/local/cargo/git
+# Install rust for the current user
+export RUSTUP_HOME=~/.rustup
+export CARGO_HOME=~/.cargo
+export RUSTUP_INIT_SKIP_PATH_CHECK=yes
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+source "$HOME/.cargo/env"
 
 # Force the installation of the git submodules
 git remote add origin https://github.com/Sunscreen-tech/parasol-rust-template.git
